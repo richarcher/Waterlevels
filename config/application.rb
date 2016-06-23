0f8efc012e683aa114dc059779989675facb6d5b
Rich Archer <rich.archer@unboxedconsulting.com>
2016-06-23 15:20:28 +0200

    Rename app to something more descriptive

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,13 @@ require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 require 'csv'
+require 'open-uri'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Pringle
+module Waterlevels
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
