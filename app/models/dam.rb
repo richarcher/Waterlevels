4293dd22b4cdb72540a529b99e4276d29eaea689
Rich Archer <rich.archer@unboxedconsulting.com>
2016-06-24 11:39:34 +0200

    Prefer to update existing dams rather than trash and start again

--- app/models/dam.rb
+++ app/models/dam.rb
@@ -12,21 +12,24 @@ class Dam < ActiveRecord::Base
   has_many :levels, dependent: :destroy
 
   def self.import(file)
-    Dam.destroy_all
+    puts "Begin import" unless Rails.env.test?
     csv = CSV.read(file, encoding: 'ISO8859-1')
     header_row = csv[2]
     header_row.each_slice(4).with_index do |dam, i|
         if (dam.length == 4)
-            d = Dam.create(name: dam[1])
+            Dam.create(name: dam[1]) if Dam.find_by_name(dam[1]).nil?
+            d = Dam.find_by_name(dam[1])
             csv.each_with_index do |row, j|
                 next if j <= 5
                 break if row[1] == '0.00'
+
                 date = DateTime.strptime(row[0], '%d-%b-%y')
                 height = row[(4*i) + 1] ||= 0
                 storage = 0
                 storage = row[(4*i) + 2].gsub(/[^0-9]/,'') unless row[(4*i) + 2].nil?
                 percentage = row[(4*i) + 3] ||= 0
-
+                next if d.levels.find_by_date(date).present?
+                puts "adding #{dam[1]} : #{date}" if d.levels.find_by_date(date).nil?  unless Rails.env.test?
                 d.levels.create(date: date, height: height, storage: storage, percentage: percentage)
             end
         end
--- spec/models/dam_spec.rb
+++ spec/models/dam_spec.rb
@@ -14,7 +14,15 @@ RSpec.describe Dam, type: :model do
   it { should respond_to :name }
   it { should have_many :levels }
 
-  let(:data) { "BULK WATER STORAGE\r  \r,WEMMERSHOEK,,,,STEENBRAS LOWER,,,\r  \r  \r  \r01-Jan-12,48.23,44 621,76.1,,20.34,23 549,70.3,\r02-Jan-12,48.21,44 571,76.0,,20.31,23 460,70.0,\r" }
+  let(:data) {  "BULK WATER STORAGE\r"\
+                "\r"\
+                ",WEMMERSHOEK,,,,STEENBRAS LOWER,,,\r"\
+                "\r"\
+                "\r"\
+                "\r"\
+                "01-Jan-12,48.23,44 621,76.1,,20.34,23 549,70.3,\r"\
+                "02-Jan-12,48.21,44 571,76.0,,20.31,23 460,70.0,\r"
+              }
 
   describe '#import' do
     before :each do
@@ -23,17 +31,22 @@ RSpec.describe Dam, type: :model do
       Dam.import('file')
     end
 
-    it 'deletes all Dam records before re-importing' do
-      expect( Dam.all.map{ |d| d.name } ).to_not include "Dam #1"
-    end
-
     it 'creates new Dam records based on imported data' do
-      expect( Dam.first.levels.count ).to eq 2
-      expect( Dam.second.levels.count ).to eq 2
+      expect( Dam.find_by_name('WEMMERSHOEK').levels.count ).to eq 2
+      expect( Dam.find_by_name('STEENBRAS LOWER').levels.count ).to eq 2
     end
 
     it 'creates levels based on imported data' do
       expect( Level.all.count ).to eq 4
+    end
+
+    it 'appends new levels to existing dams on subsequent import' do
+      olddam = Dam.find_by_name('WEMMERSHOEK')
+      newdata = data + "03-Jan-12,48.19,44 391,75.9,,20.28,23 298,69.7,\r"
+      allow(File).to receive(:open).with('file', {:universal_newline=>false, :encoding=>'ISO8859-1'}).and_return(newdata)
+      Dam.import('file')
+      expect( Dam.find_by_name('WEMMERSHOEK') ).to eq olddam
+      expect( Dam.find_by_name('WEMMERSHOEK').levels.count ).to eq 3
     end
 
   end
