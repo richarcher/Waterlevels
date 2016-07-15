cec9f76e3af47611220e1570a48424af447f7d5a
Rich Archer <rich.archer@unboxedconsulting.com>
2016-07-15 08:43:55 +0200

    Add static html page to root
    * also tighten up routing

--- app/controllers/api/v1/dams_controller.rb
+++ app/controllers/api/v1/dams_controller.rb
@@ -1,12 +1,12 @@
 module Api::V1
   class DamsController < ApiController
 
-    # GET /v1/dams
+    # GET /api/v1/dams
     def index
       render json: Dam.all
     end
 
-    # GET /v1/dams/:id
+    # GET /api/v1/dams/:id
     def show
       render json: Level.where(:dam => params[:id]).all
     end
