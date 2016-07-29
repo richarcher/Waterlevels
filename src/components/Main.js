540ce164b702ced75ab99320ba8e31ea11fda828
Rich Archer <rich.archer@unboxedconsulting.com>
2016-07-29 14:23:42 +0200

    Basic opacity transition between pages

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,20 @@
 require('normalize.css/normalize.css');
 
 import React from 'react';
+import { RouteTransition } from 'react-router-transition'
 
 class AppComponent extends React.Component {
 
   render() {
     return <div>
+    <RouteTransition
+      pathname={this.props.location.pathname}
+      atEnter={{ opacity: 0 }}
+      atLeave={{ opacity: 0 }}
+      atActive={{ opacity: 1 }}
+    >
       {this.props.children}
+    </RouteTransition>
     </div>
   }
 }
