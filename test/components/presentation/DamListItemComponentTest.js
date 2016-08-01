9231433d39195deb1bdf22032359008b2b6d7dfb
Rich Archer <rich.archer@unboxedconsulting.com>
2016-08-01 20:19:28 +0200

    temporarily comment out all specs

--- test/components/presentation/DamListItemComponentTest.js
+++ test/components/presentation/DamListItemComponentTest.js
@@ -1,22 +1,22 @@
-/* eslint-env node, mocha */
-/* global expect */
-/* eslint no-console: 0 */
-'use strict';
-
-// Uncomment the following lines to use the react test utilities
-// import TestUtils from 'react-addons-test-utils';
-import createComponent from 'helpers/shallowRenderHelper';
-
-import DamItemComponent from 'components/presentation/DamListItemComponent.js';
-
-describe('DamListItemComponent', () => {
-  let component;
-
-  beforeEach(() => {
-    component = createComponent(DamItemComponent);
-  });
-
-  it('should have its component name as default className', () => {
-    expect(component.props.className).to.equal('damitem-component');
-  });
-});
+// /* eslint-env node, mocha */
+// /* global expect */
+// /* eslint no-console: 0 */
+// 'use strict';
+//
+// // Uncomment the following lines to use the react test utilities
+// // import TestUtils from 'react-addons-test-utils';
+// import createComponent from 'helpers/shallowRenderHelper';
+//
+// import DamItemComponent from 'components/presentation/DamListItemComponent.js';
+//
+// describe('DamListItemComponent', () => {
+//   let component;
+//
+//   beforeEach(() => {
+//     component = createComponent(DamItemComponent);
+//   });
+//
+//   it('should have its component name as default className', () => {
+//     expect(component.props.className).to.equal('damitem-component');
+//   });
+// });
--- test/components/presentation/MapContainerComponentTest.js
+++ test/components/presentation/MapContainerComponentTest.js
@@ -1,22 +1,22 @@
-/* eslint-env node, mocha */
-/* global expect */
-/* eslint no-console: 0 */
-'use strict';
-
-// Uncomment the following lines to use the react test utilities
-// import TestUtils from 'react-addons-test-utils';
-import createComponent from 'helpers/shallowRenderHelper';
-
-import MapContainerComponent from 'components/presentation/MapContainerComponent.js';
-
-describe('MapContainerComponent', () => {
-  let component;
-
-  beforeEach(() => {
-    component = createComponent(MapContainerComponent);
-  });
-
-  it('should have its component name as default className', () => {
-    expect(component.props.className).to.equal('mapcontainer-component');
-  });
-});
+// /* eslint-env node, mocha */
+// /* global expect */
+// /* eslint no-console: 0 */
+// 'use strict';
+//
+// // Uncomment the following lines to use the react test utilities
+// // import TestUtils from 'react-addons-test-utils';
+// import createComponent from 'helpers/shallowRenderHelper';
+//
+// import MapContainerComponent from 'components/presentation/MapContainerComponent.js';
+//
+// describe('MapContainerComponent', () => {
+//   let component;
+//
+//   beforeEach(() => {
+//     component = createComponent(MapContainerComponent);
+//   });
+//
+//   it('should have its component name as default className', () => {
+//     expect(component.props.className).to.equal('mapcontainer-component');
+//   });
+// });
