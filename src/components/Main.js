07fa2a00670877836b4061ac86720d6ce82d38f2
Rich Archer <rich.archer@unboxedconsulting.com>
2016-08-01 16:56:35 +0200

    App layout styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,17 +3,19 @@ require('normalize.css/normalize.css');
 import React from 'react';
 import { RouteTransition } from 'react-router-transition'
 
+require('styles/App.scss');
+
 class AppComponent extends React.Component {
 
   render() {
-    return <RouteTransition
+    return <div className="app"><RouteTransition
       pathname={this.props.location.pathname}
       atEnter={{ opacity: 0 }}
       atLeave={{ opacity: 0 }}
       atActive={{ opacity: 1 }}
     >
       {this.props.children}
-    </RouteTransition>
+    </RouteTransition></div>
   }
 }
 
